359ab1af45f01f9606e0456cf3f836af1652bc5d
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-09-11 16:27:30 -0600

    Initial Commit

---
+++ Hello_World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello_World
+//
+//  Created by Enrique SFranco on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
