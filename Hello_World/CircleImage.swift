4f0bff2e2ffa69c739a6a6c979e0a684abcbaae9
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-09-11 17:43:01 -0600

    hello world

---
+++ Hello_World/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  Hello_World
+//
+//  Created by Enrique SFranco on 11/09/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- Hello_World/ContentView.swift
+++ Hello_World/ContentView.swift
@@ -7,18 +7,30 @@
 
 import SwiftUI
 
+// view protocol (contenido y diseño de la vista)
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack(alignment: .leading) { // <- flex-colum, grid-colum-1
+                Text("Enrique")
+                    .font(.title) // <- modificadores
+                    .fontWeight(.bold)
+                    .foregroundColor(.indigo)
+                HStack() { // <- flex-row
+                    Text("Joshua Tree National Park")
+                    Spacer() // <- justify-betwen
+                    Text("California")
+                }
+            }
         }
         .padding()
     }
 }
 
+// vista previa para la vista ContentView
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
