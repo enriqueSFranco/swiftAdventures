a7f5ae4394189f1d6541ceddbc917cebbb3281ef
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-06-12 19:49:50 -0600

    delete image spider man

---
+++ Hello_World/Components/BaseLabel.swift
@@ -0,0 +1,8 @@
+//
+//  BaseLabel.swift
+//  Hello_World
+//
+//  Created by Enrique SFranco on 12/06/24.
+//
+
+import Foundation
---
+++ Hello_World/Components/BaseTextField.swift
@@ -0,0 +1,8 @@
+//
+//  BaseTextField.swift
+//  Hello_World
+//
+//  Created by Enrique SFranco on 12/06/24.
+//
+
+import Foundation
